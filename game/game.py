13adc26793f836f7cba6b870cdf895feaae325a9
mtodorov95 <mtodorov95@gmail.com>
2020-11-16 11:39:58 +0200

    score

--- game/game.py
+++ game/game.py
@@ -142,6 +142,7 @@ class Main:
         self.draw_grass()
         self.fruit.draw_fruit()
         self.snake.draw_snake()
+        self.draw_score()
 
     def draw_grass(self):
         grass_color = (167, 209, 61)
@@ -158,6 +159,22 @@ class Main:
                         grass_rect = pygame.Rect(
                             col * cell_size, row*cell_size, cell_size, cell_size)
                         pygame.draw.rect(screen, grass_color, grass_rect)
+
+    def draw_score(self):
+        score_text = str(len(self.snake.body) - 3)
+        score_surface = game_font.render(score_text, True, (56, 74, 12))
+        score_x = int(cell_size * cell_number - 60)
+        score_y = int(cell_size * cell_number - 40)
+        score_rect = score_surface.get_rect(center=(score_x, score_y))
+        apple_rect = apple.get_rect(
+            midright=(score_rect.left, score_rect.centery))
+        bg_rect = pygame.Rect(apple_rect.left, apple_rect.top,
+                              apple_rect.width+score_rect.width + 10, apple_rect.height)
+
+        pygame.draw.rect(screen, (167, 209, 61), bg_rect)
+        screen.blit(score_surface, score_rect)
+        screen.blit(apple, apple_rect)
+        pygame.draw.rect(screen, (56, 74, 12), bg_rect, 2)
 
     def check_collision(self):
         if self.fruit.position == self.snake.body[0]:
@@ -184,6 +201,8 @@ screen = pygame.display.set_mode(
 clock = pygame.time.Clock()
 
 apple = pygame.image.load(os.path.join(image_dir, 'apple.png')).convert_alpha()
+game_font = pygame.font.Font(os.path.join(
+    font_dir, 'PoetsenOne-Regular.ttf'), 25)
 
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE, 150)
